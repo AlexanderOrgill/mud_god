b16ccf5cbb83ae918d34d38081355001d0766339
Alexander Orgill <alex.w.orgill@gmail.com>
2021-02-11 13:19:15 +0000

    undoing shuffle cause broken pathssss >:O

---
+++ Sprites.py
@@ -0,0 +1,18 @@
+import pygame
+
+GOLEM = pygame.image.load("new_sprites/golem.png")
+CURSOR = pygame.image.load("new_sprites/cursor.png")
+SELECTION = pygame.image.load("new_sprites/selection.png")
+
+#flora
+GRASS = pygame.image.load("new_sprites/grass.png")
+ARID_GRASS = pygame.image.load("new_sprites/arid_grass.png")
+SNOW_GRASS = pygame.image.load("new_sprites/snow_grass.png")
+
+#greater flora
+TREE = pygame.image.load("new_sprites/tree.png")
+ARID_TREE = pygame.image.load("new_sprites/arid_tree.png")
+SNOW_TREE = pygame.image.load("new_sprites/snow_tree.png")
+
+BLACK_WATER = pygame.image.load("new_sprites/black_water.png")
+BEAR = pygame.image.load("new_sprites/bear.png")
